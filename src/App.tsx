53ab2b66bf57132e4dc53535f45802c5894c1137
kkoomin <kkooming2@gmail.com>
2024-12-14 23:42:33 +0000

    feat: set initial layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import "./App.css";
-
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <main className="flex flex-col px-[40px] py-[60px] mx-auto w-[600px] min-h-screen">
+      <h1 className="text-2xl font-bold">Blocks</h1>
+      <section className="mt-5">
+        <ul>
+          <li>
+            <span>Pagination</span>
+          </li>
+          <li>
+            <span>Infinite Scroll</span>
+          </li>
+        </ul>
+      </section>
+    </main>
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      blue: "#003F8F",
+      red: "#930000",
+      white: "#F4F4F4",
+      black: "#000000",
+      "gray-black": "#333",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
